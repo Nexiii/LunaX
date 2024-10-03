9a4f077c28d4503c55bc1dc56ab7f7af375fd862
Malte | Nexiii <58604216+Nexiii@users.noreply.github.com>
2024-10-03 22:52:59 +0200

    Delete src/io/nx/LunaX/engine directory

--- src/io/nx/LunaX/engine/AbstractGame.java
+++
@@ -1,7 +0,0 @@
-package io.nx.LunaX.engine;
-
-public abstract class AbstractGame {
-	public abstract void init(GameContainer gc);
-	public abstract void update(GameContainer gc, float dt);
-	public abstract void render(GameContainer gc, Renderer r);
-}
